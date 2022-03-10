a1be33aabf559986a7df3c488ef6afd3c422c3dc
Emir Calife <emircalife@gmail.com>
2022-03-10 11:49:43 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as GithumIcon } from 'assets/img/github.svg';
+import './styles.css';
+
+function Navbar() {
+    return (
+        <header className="dsmovie-nav-content">
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/emircalife"></a>
+                    <div className="dsmovie-contact-container">
+                        <GithumIcon />
+                        <p className="dsmovie-contact-link">/emircalife</p>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
